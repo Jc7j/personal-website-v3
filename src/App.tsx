c9766adc4dcaa25524ed7abad96965f0caf97ab7
Jc7j <chiangjason19@gmail.com>
2020-07-03 19:31:12 -0700

    Created initial IconLink component and changed App.tsx to functional

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <div className="Main">
@@ -14,6 +14,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/IconLink/IconLink.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const IconLink = () => {};
+
+export default IconLink;
